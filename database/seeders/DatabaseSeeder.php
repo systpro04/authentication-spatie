c95f0d617a78e15f48b24e87ddfe93f098fdd8cf
systpro04 <systemprogrammer04.com>
2023-09-08 14:47:25 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call([
+            RoleSeeder::class,
+            AdminSeeder::class
+        ]);
+
+        for ($i = 0; $i < 5; $i++) {
+            $user = User::factory()->create();
+            $user->assignRole('User');
+        }
+    }
+}
